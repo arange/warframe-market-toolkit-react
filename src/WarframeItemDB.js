6ec99dc3a1fe3f9df78128d12555638d7945b9c2
arange <panpan2558@gmail.com>
2020-09-16 05:23:29 +1000

    Comprehensive Khora weapon list

--- src/WarframeItemDB.js
+++ src/WarframeItemDB.js
@@ -1,15 +1,6 @@
 export const MeleeWeapons = [
   {
     name: {
-      en: 'obex',
-      zh: '奥比克斯',
-    },
-    url_name:'obex',
-    disposition: 1.3,
-    isUseable: true,
-  },
-  {
-    name: {
       en: 'Dark Split-Sword',
       zh: '暗黑分合剑',
     },
@@ -28,56 +19,11 @@ export const MeleeWeapons = [
   },
   {
     name: {
-      en: 'Lacera',
-      zh: '悲痛之刃',
-    },
-    url_name:'lacera',
-    disposition: 1.31,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Bo',
-      zh: '玻之武杖',
-    },
-    url_name:'bo',
-    disposition: 1.35,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Dual Ether',
-      zh: '苍穹双剑',
-    },
-    url_name:'dual_ether',
-    disposition: 1.45,
-    isUseable: true,
-  },
-  {
-    name: {
       en: 'Kuva Shildeg',
       zh: '赤毒希尔德',
     },
     url_name:'kuva_shildeg',
     disposition: 0.75,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Volnus',
-      zh: '创伤',
-    },
-    url_name:'volnus',
-    disposition: 1.35,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Dakra Prime',
-      zh: '达克拉 prime',
-    },
-    url_name:'dakra_prime',
-    disposition: 1.3,
     isUseable: true,
   },
   {
@@ -100,56 +46,11 @@ export const MeleeWeapons = [
   },
   {
     name: {
-      en: 'Scindo',
-      zh: '分裂斩斧',
-    },
-    url_name:'scindo',
-    disposition: 1.3,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Furax',
-      zh: '弗拉克斯',
-    },
-    url_name:'furax',
-    disposition: 1.45,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Gram',
-      zh: '格拉姆',
-    },
-    url_name:'gram',
-    disposition: 1.44,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Nami Skyla',
-      zh: '海波斯库拉对剑',
-    },
-    url_name:'nami_skyla',
-    disposition: 1.3,
-    isUseable: true,
-  },
-  {
-    name: {
       en: 'Arca Titron',
       zh: '弧电振子锤',
     },
     url_name:'arca_titron',
     disposition: 1.2,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Kronen',
-      zh: '皇家拐刃',
-    },
-    url_name:'kronen',
-    disposition: 1.43,
     isUseable: true,
   },
   {
@@ -172,15 +73,6 @@ export const MeleeWeapons = [
   },
   {
     name: {
-      en: 'Ninkondi',
-      zh: '降灵追猎者',
-    },
-    url_name:'ninkondi',
-    disposition: 1.41,
-    isUseable: true,
-  },
-  {
-    name: {
       en: 'Pennant',
       zh: '尖幡',
     },
@@ -195,15 +87,6 @@ export const MeleeWeapons = [
     },
     url_name:'masseter',
     disposition: 1.15,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Karyst',
-      zh: '凯洛斯特',
-    },
-    url_name:'karst',
-    disposition: 1.35,
     isUseable: true,
   },
   {
@@ -226,47 +109,11 @@ export const MeleeWeapons = [
   },
   {
     name: {
-      en: 'Kogake',
-      zh: '科加基',
-    },
-    url_name:'kogake',
-    disposition: 1.46,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Skana',
-      zh: '空刃',
-    },
-    url_name:'skana',
-    disposition: 1.3,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Fang',
-      zh: '狼牙',
-    },
-    url_name:'fang',
-    disposition: 1.36,
-    isUseable: true,
-  },
-  {
-    name: {
       en: 'Dual Cleavers',
       zh: '斩肉双刀',
     },
     url_name:'dual_cleavers',
     disposition: 1.2,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Pangolin Sword',
-      zh: '鲮鲤剑',
-    },
-    url_name:'pangolin_sword',
-    disposition: 1.47,
     isUseable: true,
   },
   {
@@ -285,15 +132,6 @@ export const MeleeWeapons = [
     },
     url_name:'boltace',
     disposition: 1.2,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Machete',
-      zh: '马谢特砍刀',
-    },
-    url_name:'machete',
-    disposition: 1.45,
     isUseable: true,
   },
   {
@@ -334,33 +172,6 @@ export const MeleeWeapons = [
   },
   {
     name: {
-      en: 'Kesheg',
-      zh: '怯薛',
-    },
-    url_name:'kesheg',
-    disposition: 1.35,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Ripkas',
-      zh: '锐卡斯',
-    },
-    url_name:'ripkas',
-    disposition: 1.38,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Magistar',
-      zh: '执法者',
-    },
-    url_name:'magistar',
-    disposition: 1.3,
-    isUseable: true,
-  },
-  {
-    name: {
       en: 'Nikana',
       zh: '侍刃',
     },
@@ -388,20 +199,11 @@ export const MeleeWeapons = [
   },
   {
     name: {
-      en: 'Tipedo',
-      zh: '提佩多',
+      en: 'Dakra Prime',
+      zh: '达克拉Prime',
     },
-    url_name:'tipedo',
-    disposition: 1.31,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Tekko',
-      zh: '铁钩手甲',
-    },
-    url_name:'tekko',
-    disposition: 1.4,
+    url_name:'dakra_prime',
+    disposition: 1.1,
     isUseable: true,
   },
   {
@@ -460,29 +262,11 @@ export const MeleeWeapons = [
   },
   {
     name: {
-      en: 'Glaive',
-      zh: '战刃',
-    },
-    url_name:'glaive',
-    disposition: 1.3,
-    isUseable: true,
-  },
-  {
-    name: {
       en: 'Fragor',
       zh: '重击巨锤',
     },
     url_name:'fragor',
     disposition: 1.15,
-    isUseable: true,
-  },
-  {
-    name: {
-      en: 'Krohkur',
-      zh: '克鲁古尔',
-    },
-    url_name:'krohkur',
-    disposition: 1.35,
     isUseable: true,
   },
   {
@@ -503,7 +287,558 @@ export const MeleeWeapons = [
     disposition: 0.5,
     isUseable: true,
   },
+  {
+    name: {
+      en: 'Amphis',
+      zh: '诡计之刃',
+    },
+    url_name:'amphis',
+    disposition: 1.5,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Ether Daggers',
+      zh: '诡计之刃',
+    },
+    url_name:'ether_daggers',
+    disposition: 1.49,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Cronus',
+      zh: '诡计之刃',
+    },
+    url_name:'cronus',
+    disposition: 1.48,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Dark Sword',
+      zh: '诡计之刃',
+    },
+    url_name:'dark_sword',
+    disposition: 1.48,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Dual Skana',
+      zh: '诡计之刃',
+    },
+    url_name:'dual_skana',
+    disposition: 1.48,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Heat Sword',
+      zh: '诡计之刃',
+    },
+    url_name:'heat_sword',
+    disposition: 1.48,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Plasma Sword',
+      zh: '诡计之刃',
+    },
+    url_name:'plasma_sword',
+    disposition: 1.48,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Kama',
+      zh: '诡计之刃',
+    },
+    url_name:'kama',
+    disposition: 1.47,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Pangolin Sword',
+      zh: '鲮鲤剑',
+    },
+    url_name:'pangolin_sword',
+    disposition: 1.47,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Anku',
+      zh: '鲮鲤剑',
+    },
+    url_name:'anku',
+    disposition: 1.46,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Kogake',
+      zh: '科加基',
+    },
+    url_name:'kogake',
+    disposition: 1.46,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Ankyros',
+      zh: '科加基',
+    },
+    url_name:'ankyros',
+    disposition: 1.45,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Dual Ether',
+      zh: '苍穹双剑',
+    },
+    url_name:'dual_ether',
+    disposition: 1.45,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Ether Reaper',
+      zh: '苍穹双剑',
+    },
+    url_name:'ether_reaper',
+    disposition: 1.45,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Kestrel',
+      zh: '苍穹双剑',
+    },
+    url_name:'kestrel',
+    disposition: 1.45,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Machete',
+      zh: '马谢特砍刀',
+    },
+    url_name:'machete',
+    disposition: 1.45,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Furax',
+      zh: '弗拉克斯',
+    },
+    url_name:'furax',
+    disposition: 1.45,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'DUAL ZOREN',
+      zh: '苍穹双剑',
+    },
+    url_name:'dual_zoren',
+    disposition: 1.44,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'ETHER SWORD',
+      zh: '苍穹双剑',
+    },
+    url_name:'ether_sword',
+    disposition: 1.44,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Gram',
+      zh: '格拉姆',
+    },
+    url_name:'gram',
+    disposition: 1.44,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'HALIKAR',
+      zh: '格拉姆',
+    },
+    url_name:'halikar',
+    disposition: 1.44,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'HEAT DAGGER',
+      zh: '格拉姆',
+    },
+    url_name:'heat_dagger',
+    disposition: 1.44,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'CERAMIC DAGGER',
+      zh: '格拉姆',
+    },
+    url_name:'ceramic_dagger',
+    disposition: 1.43,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Kronen',
+      zh: '皇家拐刃',
+    },
+    url_name:'kronen',
+    disposition: 1.43,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'NAMI SOLO',
+      zh: '皇家拐刃',
+    },
+    url_name:'nami_solo',
+    disposition: 1.43,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Ninkondi',
+      zh: '降灵追猎者',
+    },
+    url_name:'ninkondi',
+    disposition: 1.41,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'DUAL RAZA',
+      zh: '降灵追猎者',
+    },
+    url_name:'dual_raza',
+    disposition: 1.40,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'JAW SWORD',
+      zh: '降灵追猎者',
+    },
+    url_name:'jaw_sword',
+    disposition: 1.40,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Bo',
+      zh: '玻之武杖',
+    },
+    url_name:'bo',
+    disposition: 1.40,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'OKINA',
+      zh: '玻之武杖',
+    },
+    url_name:'okina',
+    disposition: 1.40,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'PROVA',
+      zh: '玻之武杖',
+    },
+    url_name:'prova',
+    disposition: 1.40,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Tekko',
+      zh: '铁钩手甲',
+    },
+    url_name:'tekko',
+    disposition: 1.4,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Ripkas',
+      zh: '锐卡斯',
+    },
+    url_name:'ripkas',
+    disposition: 1.38,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'SERRO',
+      zh: '锐卡斯',
+    },
+    url_name:'serro',
+    disposition: 1.38,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'TONBO',
+      zh: '锐卡斯',
+    },
+    url_name:'tonbo',
+    disposition: 1.38,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'CERATA',
+      zh: '锐卡斯',
+    },
+    url_name:'cerata',
+    disposition: 1.36,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Fang',
+      zh: '狼牙',
+    },
+    url_name:'fang',
+    disposition: 1.36,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'BROKEN SCEPTER',
+      zh: '狼牙',
+    },
+    url_name:'broken_scepter',
+    disposition: 1.35,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Karyst',
+      zh: '凯洛斯特',
+    },
+    url_name:'karst',
+    disposition: 1.35,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Kesheg',
+      zh: '怯薛',
+    },
+    url_name:'kesheg',
+    disposition: 1.35,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Krohkur',
+      zh: '克鲁古尔',
+    },
+    url_name:'krohkur',
+    disposition: 1.35,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'PROVA VANDAL',
+      zh: '克鲁古尔',
+    },
+    url_name:'prova_vandal',
+    disposition: 1.35,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'SHAKU',
+      zh: '克鲁古尔',
+    },
+    url_name:'shaku',
+    disposition: 1.35,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'SIBEAR',
+      zh: '克鲁古尔',
+    },
+    url_name:'sibear',
+    disposition: 1.35,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Volnus',
+      zh: '创伤',
+    },
+    url_name:'volnus',
+    disposition: 1.35,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Lacera',
+      zh: '悲痛之刃',
+    },
+    url_name:'lacera',
+    disposition: 1.31,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Tipedo',
+      zh: '提佩多',
+    },
+    url_name:'tipedo',
+    disposition: 1.31,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'CAUSTACYST',
+      zh: '提佩多',
+    },
+    url_name:'caustacyst',
+    disposition: 1.31,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Dex Dakra',
+      zh: 'Dex达克拉',
+    },
+    url_name:'dex_dakra',
+    disposition: 1.3,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'ENDURA',
+      zh: 'Dex达克拉',
+    },
+    url_name:'endura',
+    disposition: 1.3,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Glaive',
+      zh: '战刃',
+    },
+    url_name:'glaive',
+    disposition: 1.3,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'Magistar',
+      zh: '执法者',
+    },
+    url_name:'magistar',
+    disposition: 1.3,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'MIRE',
+      zh: '执法者',
+    },
+    url_name:'mire',
+    disposition: 1.3,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Nami Skyla',
+      zh: '海波斯库拉对剑',
+    },
+    url_name:'nami_skyla',
+    disposition: 1.3,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'obex',
+      zh: '奥比克斯',
+    },
+    url_name:'obex',
+    disposition: 1.3,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'ORVIUS',
+      zh: '奥比克斯',
+    },
+    url_name:'orvius',
+    disposition: 1.3,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Scindo',
+      zh: '分裂斩斧',
+    },
+    url_name:'scindo',
+    disposition: 1.3,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'SCOLIAC',
+      zh: '分裂斩斧',
+    },
+    url_name:'scoliac',
+    disposition: 1.3,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'SHEEV',
+      zh: '分裂斩斧',
+    },
+    url_name:'sheev',
+    disposition: 1.3,
+    isUseable: false,
+  },
+  {
+    name: {
+      en: 'Skana',
+      zh: '空刃',
+    },
+    url_name:'skana',
+    disposition: 1.3,
+    isUseable: true,
+  },
+  {
+    name: {
+      en: 'TWIN BASOLK',
+      zh: '空刃',
+    },
+    url_name:'twin_basolk',
+    disposition: 1.3,
+    isUseable: false,
+  },
+  
 ];
+// 1.25 ACK &amp; BRUNT</li><li>1.25 CASSOWAR</li><li>1.25 COBRA &amp; CRANE</li><li>1.25 DECONSTRUCTOR</li><li>1.25 DECONSTRUCTOR PRIME</li><li>1.25 DESTREZA</li><li>1.25 FALCOR</li><li>1.25 GALVACORD</li><li>1.25 GUNSEN</li><li>1.25 HELIOCOR</li><li>1.25 KORRUDO</li><li>1.25 KRESKA</li><li>1.25 KRONSH</li><li>1.25 PATHOCYST</li><li>1.25 PUPACYST</li><li>1.25 SCINDO PRIME</li><li>1.25 TIPEDO PRIME</li><li>1.25 TWIN KROHKUR</li><li>1.24 SARPA</li><li>1.22 GLAIVE PRIME</li><li>1.21 DARK SPLIT-SWORD</li><li>1.20 ARCA TITRON</li><li>1.20 BOLTACE</li><li>1.20 DRAGON NIKANA</li><li>1.20 DUAL CLEAVERS</li><li>1.20 DUAL KERES</li><li>1.20 HATE</li><li>1.20 MIOS</li><li>1.20 PRISMA OBEX</li><li>1.20 PRISMA SKANA</li><li>1.20 RABVEE</li><li>1.20 SANCTI MAGISTAR</li><li>1.20 SKANA PRIME</li><li>1.20 SYNOID HELIOCOR</li><li>1.20 WOLF SLEDGE</li><li>1.17 REDEEMER</li><li>1.16 DUAL ICHOR</li><li>1.15 DEHTAT</li><li>1.15 DUAL KAMAS</li><li>1.15 FRAGOR</li><li>1.15 JAT KITTAG</li><li>1.15 MASSETER</li><li>1.15 OHMA</li><li>1.15 OOLTHA</li><li>1.15 SILVA &amp; AEGIS</li><li>1.15 SYDON</li><li>1.14 DESTREZA PRIME</li><li>1.10 DAKRA PRIME</li><li>1.10 JAT KUSAR</li><li>1.10 NINKONDI PRIME</li><li>1.10 PRISMA DUAL CLEAVERS</li><li>1.10 VAYKOR SYDON</li><li>1.05 BROKEN WAR</li><li>1.05 CETI LACERA</li><li>1.05 DUAL KAMAS PRIME</li><li>1.05 FRAGOR PRIME</li><li>1.05 GRAM PRIME</li><li>1.05 KRONEN PRIME</li><li>1.05 MEWAN</li><li>1.00 GUANDAO</li><li>1.00 LECTA</li><li>1.00 NAMI SKYLA PRIME</li><li>1.00 PANGOLIN PRIME</li><li>1.00 REAPER PRIME</li><li>1.00 SIGMA &amp; OCTANTIS</li><li>1.00 SILVA &amp; AEGIS PRIME</li><li>1.00 TELOS BOLTACE</li><li>0.95 HIRUDO</li><li>0.95 TATSU</li><li>0.95 ZENISTAR</li><li>0.90 ATTERAX</li><li>0.90 BALLA</li><li>0.90 CYATH</li><li>0.90 DARK DAGGER</li><li>0.90 SECURA LECTA</li><li>0.90 SKIAJATI</li><li>0.90 VENKA</li><li>0.90 WAR</li><li>0.85 GALATINE</li><li>0.85 ORTHOS</li><li>0.85 PENNANT</li><li>0.80 NIKANA</li><li>0.80 RAKTA DARK DAGGER</li><li>0.80 REDEEMER PRIME</li><li>0.80 VENKA PRIME</li><li>0.75 DOKRAHM</li><li>0.75 KUVA SHILDEG</li><li>0.75 PLAGUE KEEWAR</li><li>0.75 SEPFAHN</li><li>0.70 PARACESIS</li><li>0.65 GALATINE PRIME</li><li>0.65 LESION</li><li>0.65 PLAGUE KRIPATH</li><li>0.60 ORTHOS PRIME</li><li>0.50 KARYST PRIME</li><li>0.50 KERATINOS</li><li>0.50 NIKANA PRIME</li><li>0.50 QUASSUS</li><li>0.50 STROPHA</li><li>0.50 XORIS</li></ul>
 
 export const ATTRIBUTES = {
   baseDamage: { name: 'base_damage_/_melee_damage', zh: '基础伤害'},
